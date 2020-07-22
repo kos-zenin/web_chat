009dc5f1062e11f91f28100b6d7e2ae60974061b
kos-zenin <kos.zenin@gmail.com>
2020-07-22 17:45:13 +0100

    Add users controller implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    user = User.new(permitted_params)
+
+    if user.save
+      session[:user_id] = user.id
+
+      redirect_to root_path
+    else
+      @user = user || User.new
+
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
