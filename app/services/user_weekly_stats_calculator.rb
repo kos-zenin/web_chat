659c7b08a0891d9400c824d3c99b7e3e451fd1e2
kos-zenin <kos.zenin@gmail.com>
2020-07-23 10:53:54 +0100

    Refactor weekly stats to use DI

--- app/services/user_weekly_stats_calculator.rb
+++ app/services/user_weekly_stats_calculator.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class UserWeeklyStatsCalculator
-  def initialize(user)
+  def initialize(user, chat: CurrentChat.new.load)
     @user = user
+    @chat = chat
   end
 
   def call
@@ -15,18 +16,14 @@ class UserWeeklyStatsCalculator
 
   private
 
-  def chat
-    @chat ||= CurrentChat.new.load
-  end
-
   def number_of_messages_during_last_week
-    chat.messages.in_period(from: 1.week.ago, to: Time.current).count
+    @chat.messages.in_period(from: 1.week.ago, to: Time.current).count
   end
 
   def number_of_messages_since_your_last_message
     return if last_message_created_at.blank?
 
-    chat.messages.in_period(from: last_message_created_at, to: Time.current).count
+    @chat.messages.in_period(from: last_message_created_at, to: Time.current).count
   end
 
   def last_message_created_at
